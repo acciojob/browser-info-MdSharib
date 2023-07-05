904e4d8061b99e33590849e50432043ed7868c4e
Md Sharib <109461910+MdSharib@users.noreply.github.com>
2023-07-05 18:27:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const div =  document.getElementById("browser-info");
+
+
+const text = `You are using ${navigator.appName} version ${navigator.appVersion}`
+
+
+div.innerText = text;
